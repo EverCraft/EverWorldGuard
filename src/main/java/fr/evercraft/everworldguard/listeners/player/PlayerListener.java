3739a5a025b4ca081e4072351f79dd71a3b1379b
rexbut <butbutdu45@gmail.com>
2017-03-11 07:05:28 +0100

    Update

--- src/main/java/fr/evercraft/everworldguard/listeners/player/PlayerListener.java
+++ src/main/java/fr/evercraft/everworldguard/listeners/player/PlayerListener.java
@@ -17,16 +17,22 @@
 package fr.evercraft.everworldguard.listeners.player;
 
 import java.util.Optional;
+import java.util.stream.Stream;
 
+import org.spongepowered.api.data.key.Keys;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.Order;
 import org.spongepowered.api.event.block.ChangeBlockEvent;
+import org.spongepowered.api.event.cause.NamedCause;
 import org.spongepowered.api.event.cause.entity.damage.source.EntityDamageSource;
 import org.spongepowered.api.event.entity.DamageEntityEvent;
 import org.spongepowered.api.event.entity.HealEntityEvent;
 import org.spongepowered.api.event.network.ClientConnectionEvent;
+import org.spongepowered.api.util.Direction;
+import org.spongepowered.api.world.LocatableBlock;
 
+import com.flowpowered.math.vector.Vector3d;
 import fr.evercraft.everapi.services.worldguard.WorldWorldGuard;
 import fr.evercraft.everapi.services.worldguard.flag.Flags;
 import fr.evercraft.everapi.services.worldguard.flag.type.StateFlag.State;
@@ -58,7 +64,56 @@ public class PlayerListener {
 	}
 	
 	@Listener(order=Order.FIRST)
-	public void onChangeBlockBreak(ChangeBlockEvent event) {
+	public void onChangeBlockPre(ChangeBlockEvent.Pre event) {
+		if (event.isCancelled()) return;
+
+		Optional<LocatableBlock> piston = event.getCause().get(NamedCause.SOURCE, LocatableBlock.class);
+		if (piston.isPresent() && event.getCause().containsNamed(NamedCause.PISTON_EXTEND)) {
+			this.onChangeBlockPrePiston(event, piston.get());
+			return;
+		}
+		
+		WorldWorldGuard world = this.plugin.getProtectionService().getOrCreateWorld(event.getTargetWorld());
+		Optional<Player> optPlayer = event.getCause().first(Player.class);
+		if (optPlayer.isPresent()) {
+			Player player = optPlayer.get();
+			if (event.getLocations().stream().filter(location -> world.getRegions(location.getPosition()).getFlag(player, Flags.BUILD).equals(State.DENY))
+				.count() > 0) {
+				event.setCancelled(true);
+			}
+		} else {
+			if (event.getLocations().stream().filter(location -> world.getRegions(location.getPosition()).getFlagDefault(Flags.BUILD).equals(State.DENY))
+				.count() > 0) {
+				event.setCancelled(true);
+			}
+		}
+	}
+	
+	public void onChangeBlockPrePiston(ChangeBlockEvent.Pre event, LocatableBlock block) {
+		WorldWorldGuard world = this.plugin.getProtectionService().getOrCreateWorld(event.getTargetWorld());
+		Optional<Player> optPlayer = event.getCause().first(Player.class);
+		Vector3d direction = block.get(Keys.DIRECTION).orElse(Direction.NONE).asOffset();
+		
+		Stream<Vector3d> positions = Stream.concat(
+				event.getLocations().stream().map(location -> location.getPosition()),
+				event.getLocations().stream().map(location -> location.getPosition().add(direction)))
+				.distinct();
+		
+		if (optPlayer.isPresent()) {
+			Player player = optPlayer.get();
+			
+			positions.filter(position -> world.getRegions(position).getFlag(player, Flags.BUILD).equals(State.DENY))
+				.findAny()
+				.ifPresent(position -> event.setCancelled(true));
+		} else {
+			positions.filter(position -> world.getRegions(position).getFlagDefault(Flags.BUILD).equals(State.DENY))
+				.findAny()
+				.ifPresent(position -> event.setCancelled(true));
+		}
+	}
+	
+	@Listener(order=Order.FIRST)
+	public void onChangeBlock(ChangeBlockEvent event) {
 		if (event.isCancelled()) return;
 		
 		Optional<Player> optPlayer = event.getCause().first(Player.class);
@@ -69,20 +124,18 @@ public class PlayerListener {
 		}
 	}
 	
-	public void onChangeBlockPlayer(ChangeBlockEvent event, Player player) {
-		//player.sendMessage(Text.of(String.join(", ", event.getCause().getNamedCauses().keySet())));
-		event.getTransactions().forEach(transaction -> {
-		});
-		
+	public void onChangeBlockPlayer(ChangeBlockEvent event, Player player) {		
 		WorldWorldGuard world = this.plugin.getProtectionService().getOrCreateWorld(event.getTargetWorld());		
-		if (event instanceof ChangeBlockEvent.Pre || event instanceof ChangeBlockEvent.Place || event instanceof ChangeBlockEvent.Break) {
-			event.filter(location -> world.getRegions(location.getPosition()).getFlag(player, Flags.BUILD).equals(State.ALLOW));
+		
+		if (event instanceof ChangeBlockEvent.Place || event instanceof ChangeBlockEvent.Break) {
+			event.filter(location -> world.getRegions(location.getPosition()).getFlag(player, Flags.BUILD).equals(State.ALLOW)).isEmpty();
 		}
 	}
 	
 	public void onChangeBlockNatural(ChangeBlockEvent event) {
 		WorldWorldGuard world = this.plugin.getProtectionService().getOrCreateWorld(event.getTargetWorld());
-		if (event instanceof ChangeBlockEvent.Pre || event instanceof ChangeBlockEvent.Place || event instanceof ChangeBlockEvent.Break) {
+		
+		if (event instanceof ChangeBlockEvent.Place || event instanceof ChangeBlockEvent.Break) {
 			event.filter(location -> world.getRegions(location.getPosition()).getFlagDefault(Flags.BUILD).equals(State.ALLOW));
 		}
 	}
